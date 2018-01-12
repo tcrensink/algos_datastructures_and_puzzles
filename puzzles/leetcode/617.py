6035c68dfb2000a0d142f8fe7f00d2daab900af6
TR <thomas.rensink@gmail.com>
2018-01-12 14:19:24 -0800

    modified leetcode example

--- puzzles/leetcode/617.py
+++ puzzles/leetcode/617.py
@@ -1,15 +1,14 @@
 """
-Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
-
-You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+https://leetcode.com/problems/merge-two-binary-trees/description/
 """
+
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
-        
+
 class Solution:
     def mergeTrees(self, t1, t2):
         """
@@ -34,5 +33,4 @@ class Solution:
         t.left = self.mergeTrees(t1.left, t2.left)
         t.right = self.mergeTrees(t1.right, t2.right)
         return t
-        
         
