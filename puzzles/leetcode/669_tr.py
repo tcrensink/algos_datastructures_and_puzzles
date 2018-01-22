9d897c5aa546cf1f280a76bc5d0efa17429b7ed4
TR <thomas.rensink@gmail.com>
2018-01-21 20:23:23 -0800

    added solution 669_tr.py

---
+++ puzzles/leetcode/669_tr.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """  
+        # stopping condition:
+        if root is None:
+            return None
+        
+        # trim R, L
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        if root.val < L:
+            return self.trimBST(root.right, L, R)
+        
+        # branch
+        else:
+            root.left = self.trimBST(root.left, L, R)
+            root.right = self.trimBST(root.right, L, R)
+            return root
+        
+        
---
+++ recursion/tree_traversal.py
@@ -0,0 +1,45 @@
+import random
+"""
+Generate a binary tree and write a few recursive algorithms to traverse it, performing various functions:
+"""
+
+# Tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# Randomly generated tree (not BST)
+class Tree:
+	def __init__(self):
+		self.root = None
+
+		for j in node_vals:
+
+			self.left = TreeNode(node_vals.pop)
+			self.right = TreeNode(node_vals.pop)
+
+
+
+
+class Solution:
+    def reproduceTree(self, root):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if root.val is None:
+            return None
+        
+        if root.val < L or root.val > R:
+        
+            return self.trimBST(root.left, L, R)
+            return self.trimBST(root.right,L, R)
+        
+        else:
+            return self.trimBST(root.left, L, R)
+            return self.trimBST(root.right,L, R)
+        
